af221e47603e723c4a94482c1c6f46cbe113f0a9
Gurupriyan j <gurupriyanj007@gmail.com>
2025-01-31 16:52:18 +0530

    initial commit

---
+++ src/components/semntics/Browse.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import ContentArea from "./ContentArea";
+import { getUserCredential, publicRoutes } from "../../common/utils";
+
+function Browse() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const userData = getUserCredential();
+  const getBasePath = (pathname) => {
+    const pathParts = pathname.split("/").filter(Boolean);
+    return `/${pathParts[0]}`;
+  };
+  const basePath = getBasePath(location.pathname);
+
+  const parseJwt = () => {
+    try {
+      return JSON.parse(atob(userData.split(".")[1]));
+    } catch (e) {
+      return null;
+    }
+  };
+  useEffect(() => {
+    if (publicRoutes.includes(basePath)) {
+      if (userData) {
+        const decodedJwt = parseJwt(userData);
+        if (decodedJwt.exp * 1000 < Date.now()) {
+          try {
+            navigate("/login");
+          } catch (error) {
+            console.log(error.response.data.message);
+          }
+        } else {
+          navigate("/");
+        }
+      } else {
+        navigate(location.pathname);
+      }
+    } else {
+      if (!userData) {
+        navigate("/login");
+      }
+    }
+    //eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    // Function to detect language and apply appropriate classes
+    const applyLanguageClasses = () => {
+      document.querySelectorAll("p, span, div").forEach((element) => {
+        const text = element.textContent;
+
+        // Arabic character range
+        if (/[ء-ي]/.test(text)) {
+          element.classList.add("arabic-text");
+        }
+        // Malayalam character range
+        else if (/[\u0D00-\u0D7F]/.test(text)) {
+          element.classList.add("malayalam-text");
+        }
+        // Default to English
+        else {
+          element.classList.add("english-text");
+        }
+      });
+    };
+
+    // Apply classes initially
+    applyLanguageClasses();
+
+    // Apply classes whenever content updates
+    const observer = new MutationObserver(applyLanguageClasses);
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // Cleanup observer on component unmount
+    return () => observer.disconnect();
+  }, []);
+
+  if (publicRoutes.includes(basePath)) {
+    return (
+      <div className="flex-1 flex flex-col">
+        <ContentArea />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col h-screen">
+        <Header
+          className="w-full fixed top-0 left-0 z-10"
+          toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
+        />
+
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+
+          <main
+            className={`flex-1 overflow-y-auto  p-4 ${
+              location.pathname === "/" ? "bg-white" : "bg-[#CFCFCF]"
+            }`}
+          >
+            <ContentArea />
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Browse;
---
+++ src/constants/tableData.js
@@ -0,0 +1,453 @@
+export const vendorData = [
+  {
+    slNo: 1,
+    id: "VEN0001",
+    name: "FreshMart Groceries",
+    image: "/constants/vendor.png",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 2,
+    id: "VEN0002",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 3,
+    id: "VEN0003",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 4,
+    id: "VEN0004",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 5,
+    id: "VEN0005",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 6,
+    id: "VEN0006",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 7,
+    id: "VEN0007",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 8,
+    id: "VEN0008",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 9,
+    id: "VEN0009",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 10,
+    id: "VEN0010",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  }
+  //   {
+  //     slNo: 11,
+  //     id: "VEN0011",
+  //     image: "/constants/vendor.png",
+  //      name:"FreshMart Groceries",
+  //     description:
+  //       "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+  //     status: true
+  //   },
+  //   {
+  //     slNo: 12,
+  //     id: "VEN0012",
+  //     image: "/constants/vendor.png",
+  //      name:"FreshMart Groceries",
+  //     description:
+  //       "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+  //     status: true
+  //   },
+  //   {
+  //     slNo: 13,
+  //     id: "VEN0013",
+  //     image: "/constants/vendor.png",
+  //      name:"FreshMart Groceries",
+  //     description:
+  //       "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+  //     status: true
+  //   },
+  //   {
+  //     slNo: 14,
+  //     id: "VEN0014",
+  //     image: "/constants/vendor.png",
+  //      name:"FreshMart Groceries",
+  //     description:
+  //       "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+  //     status: true
+  //   },
+  //   {
+  //     slNo: 15,
+  //     id: "VEN0015",
+  //     image: "/constants/vendor.png",
+  //      name:"FreshMart Groceries",
+  //     description:
+  //       "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+  //     status: true
+  //   },
+];
+
+export const VendorCategoryData = [
+  {
+    slNo: 1,
+    id: "VEN0001",
+    name: "FreshMart Groceries",
+    image: "/constants/vendor.png",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 2,
+    id: "VEN0002",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 3,
+    id: "VEN0003",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 4,
+    id: "VEN0004",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 5,
+    id: "VEN0005",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 6,
+    id: "VEN0006",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 7,
+    id: "VEN0007",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 8,
+    id: "VEN0008",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 9,
+    id: "VEN0009",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 10,
+    id: "VEN0010",
+    image: "/constants/vendor.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  }
+];
+export const MenuCategoryData = [
+  {
+    slNo: 1,
+    id: "MCCD001",
+    name: "Main Course",
+    image: "/constants/category.png",
+    description:
+      "The centerpiece of every meal, featuring bold flavors and satisfying portions, designed to delight and fulfill your appetite.",
+    status: true
+  },
+  {
+    slNo: 2,
+    id: "MCCD002",
+    image: "/constants/category.png",
+    name: "Starters",
+    description:
+      "Starters are the perfect way to begin your meal, offering a variety of small, flavorful dishes that awaken the palate. From crispy, savory bites to light, refreshing options, starters set the stage for the main course and add excitement to the dining experience.",
+    status: false
+  },
+  {
+    slNo: 3,
+    id: "MCCD003",
+    image: "/constants/category.png",
+    name: "Desserts",
+    description:
+      "Sweet Treats are the perfect way to end any meal, offering a variety of indulgent flavors and textures. Whether rich and creamy or light and refreshing, these delights provide a satisfying finish that leaves you with a smile.",
+    status: true
+  },
+  {
+    slNo: 4,
+    id: "MCCD004",
+    image: "/constants/category.png",
+    name: "Breakfast",
+    description:
+      "The most important meal of the day, breakfast provides a nourishing start with a variety of hearty, flavorful options. From classic egg dishes and pancakes to fresh fruit and pastries, breakfast fuels you for the day ahead.",
+    status: false
+  },
+  {
+    slNo: 5,
+    id: "MCCD005",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 6,
+    id: "MCCD006",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 7,
+    id: "MCCD007",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 8,
+    id: "MCCD008",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 9,
+    id: "MCCD009",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 10,
+    id: "MCCD010",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  }
+];
+export const MenuSubCategoryData = [
+  {
+    slNo: 1,
+    id: "MCCD001",
+    name: "Main Course",
+    image: "/constants/category.png",
+    description:
+      "The centerpiece of every meal, featuring bold flavors and satisfying portions, designed to delight and fulfill your appetite.",
+    status: true
+  },
+  {
+    slNo: 2,
+    id: "MCCD002",
+    image: "/constants/category.png",
+    name: "Starters",
+    description:
+      "Starters are the perfect way to begin your meal, offering a variety of small, flavorful dishes that awaken the palate. From crispy, savory bites to light, refreshing options, starters set the stage for the main course and add excitement to the dining experience.",
+    status: false
+  },
+  {
+    slNo: 3,
+    id: "MCCD003",
+    image: "/constants/category.png",
+    name: "Desserts",
+    description:
+      "Sweet Treats are the perfect way to end any meal, offering a variety of indulgent flavors and textures. Whether rich and creamy or light and refreshing, these delights provide a satisfying finish that leaves you with a smile.",
+    status: true
+  },
+  {
+    slNo: 4,
+    id: "MCCD004",
+    image: "/constants/category.png",
+    name: "Breakfast",
+    description:
+      "The most important meal of the day, breakfast provides a nourishing start with a variety of hearty, flavorful options. From classic egg dishes and pancakes to fresh fruit and pastries, breakfast fuels you for the day ahead.",
+    status: false
+  },
+  {
+    slNo: 5,
+    id: "MCCD005",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 6,
+    id: "MCCD006",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 7,
+    id: "MCCD007",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 8,
+    id: "MCCD008",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: false
+  },
+  {
+    slNo: 9,
+    id: "MCCD009",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  },
+  {
+    slNo: 10,
+    id: "MCCD010",
+    image: "/constants/category.png",
+    name: "FreshMart Groceries",
+    description:
+      "FreshMart Groceries is a trusted supplier of farm-fresh fruits and vegetables, organic produce, and daily essentials. Known for timely deliveries and premium quality products, they cater to both individual customers and businesses.",
+    status: true
+  }
+];
+
+export const PlansTableData =[
+  {
+    sl:1,
+    name:"Basic",
+    id:"BP-001",
+    image:"/constants/planIcon2.png",
+    actualPrice:999,
+    offerPrice:799,
+    description:"Manage up to 5 restaurants ,Basic analytics dashboard ,Daily revenue reports ,Email notifications ,10GB cloud storage for backups ,Email support",
+    validity:1,
+    validityType:"month",
+    status:true
+  },
+  {
+    sl:1,
+    name:"Pro",
+    id:"PP-002",
+    image:"/constants/planIcon.png",
+    actualPrice:2999,
+    offerPrice:2499,
+    description:"Manage up to 5 restaurants ,Basic analytics dashboard ,Daily revenue reports ,Email notifications ,10GB cloud storage for backups ,Email support",
+    validity:6,
+    validityType:"month",
+    status:false
+  },
+  {
+    sl:1,
+    name:"Enterprise",
+    id:"EP-002",
+    image:"/constants/planIcon2.png",
+    actualPrice:7999,
+    offerPrice:6999,
+    description:"Manage up to 5 restaurants ,Basic analytics dashboard ,Daily revenue reports ,Email notifications ,10GB cloud storage for backups ,Email support",
+    validity:1,
+    validityType:"year",
+    status:true
+  },
+]
